196760caa8653e3c9f61d78efaef728643d642a3
valentim pereira <zadinho72@gmail.com>
2023-08-02 06:04:58 -0300

    upando na page

--- .vscode/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMode() {
-  const html = document.documentElement
-  if(html.classList.contains('light')){
-     html.classList.remove('light')
-  } else{
-    html.classList.add('light')
-  }
-} 
-
